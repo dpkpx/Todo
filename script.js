a3f01244aec426ef7d1e9e4d6c160a29926f1473
dpkpx <contactdpk1@gmail.com>
2022-07-31 19:21:17 +0530

    update

--- script.js
+++ script.js
@@ -1,294 +1,215 @@
-//upadating from local storage
-update(false);
+let timer;
+let tempCurrentTodo;
+let todos = {};
+let doNotAnimation = false
+let animate = true
+
+// ===================== flag ====================
+let editMode = false;
+// ===============================================
+updateScreen();
+
+let todosWrapper = document.querySelector(".todosWrapper");
+
+todosWrapper.addEventListener("pointerdown", pointerDown);
+
+document.querySelector(".inputWrapper form").addEventListener("submit", handelSubmit)
 
 
-//getting input on submit
-document.querySelector('#form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    add();
+todosWrapper.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    let todo = e.target.closest(".todo")
+    if (!todo) return;
+    showMenu(todo);
+    todo.querySelector("input[type='checkbox']").disabled = true;
 });
 
-//object  to receive data
-let data = {
-    key: "0",
-    input: '',
-    isChecked: false,
-}
 
-//function to add todo to local storage and Dom
-function add() {
-
-    //scrolls to the top
-    document.documentElement.scrollTop = 0;
-
-    let todo = document.querySelector('#inputbox').value
-
-    if (todo) {
-
-
-        //generating key by parsing the id of first element to integer and then adding 1 to it 
-        try {
-            data.key = (parseInt(document.querySelector('#uList').children[0].children[0].children[0].children[0].id.replace('c', '')) + 1).toString();
-        }
-        catch (err) {
-            //if todo list is empty
-            data.key = "0";
-        }
-
-        data.input = todo;
-        data.isChecked = false;
-
-        //storing data on local storage
-        localStorage.setItem(data.key, JSON.stringify(data));
-
-        //adding todo to the screen
-        addRecord(data.key, data.input, data.isChecked, true);
-
-        //clearing the input field
-        document.querySelector('#inputbox').value = "";
-
-
-    }
+function pointerDown(e) {
+    clearTimer();
+    let todo = e.target.closest(".todo")
+    if (!todo) return;
+    timer = setTimeout(() => {
+        showMenu(todo);
+        todo.querySelector("input[type='checkbox']").disabled = true;
+    }, 1300)
+    document.body.addEventListener("pointerup", clearTimer, { once: true })
 }
 
 
-
-// removing the checked items
-function remove() {
-    let temp = document.querySelector('#uList');
-    for (let index = 0; index < temp.children.length; index++) {
-        if (temp.children[index].children[0].children[0].children[0].checked) {
-            localStorage.removeItem(temp.children[index].children[0].children[0].children[0].id.replace('c', ''));
-
-            temp.children[index].className = "listWithAnimation";
-
-            setTimeout(function () {
-                temp.removeChild(temp.children[temp.children.length - 1])
-            }, 350);
-
-
-        }
-    }
+function clearTimer() {
+    if (!timer) return
+    clearTimeout(timer)
 }
 
-//function for updating from localstorage
-function update(animate) {
-    //deleting all todos from DOM
-    let temp = document.querySelector('#uList');
-    temp.textContent = '';
+
+function showMenu(todo) {
+    let contextMenu = todo.querySelector(".contextMenu")
+    contextMenu.style.display = "block"
+    contextMenu.focus();
+    contextMenu.addEventListener("focusout", () => {
+        contextMenu.style = "display:block;opacity:0"
+        setTimeout(() => {
+            contextMenu.style = "display:none"
+            todo.querySelector("input[type='checkbox']").disabled = false;
+        }, 500)
+    }, { once: true })
+}
 
 
-    //recreating all todos  in DOM
-    let keys = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        keys.push(localStorage.key(i));
-    }
-    keys.sort((a, b) =>  a - b );
-
-
-    for (let index of keys) {
-        addRecord(JSON.parse(localStorage.getItem(index)).key, JSON.parse(localStorage.getItem(index)).input, JSON.parse(localStorage.getItem(index)).isChecked, animate)
-
-    }
-
+function editTodo(editButton) {
+    editMode = true;
+    let todo = editButton.parentElement.parentElement
+    let todoText = todo.querySelector(".todoText").innerText
+    let inputBox = document.querySelector('#inputbox')
+    tempCurrentTodo = todo
+    inputBox.focus();
+    inputBox.value = todoText
 
 }
 
-//boolean to stop users from cleacking multiple time on same element
-//(cleacking multiple time on same element while animating results duplicate list items)
-let clickAvailable = true;
 
-//function to bubble up or down lists when checked or unchecked
-function checkdone(checkbox) {
-    if (clickAvailable) {
+function deleteTodo(deleteButton) {
+    let todo = deleteButton.parentElement.parentElement
+    todo.parentElement.removeChild(todo)
+    let todoId = todo.querySelector("input[type='checkbox']").id.replace("c", "")
+    delete todos[todoId]
 
-
-        let key = checkbox.id.replace('c', '');
-        let temp = JSON.parse(localStorage.getItem(key));
-
-
-        data.key = key;
-        data.input = temp.input;
-        data.isChecked = checkbox.checked;
-
-
-        let tempData = {
-            key: "0",
-            input: '',
-            isChecked: false,
-        }
-
-        //getting all available keys from localStorage
-        let keys = [];
-
-        for (let i = 0; i < localStorage.length; i++) {
-            keys.push(localStorage.key(i));
-        }
-        keys.sort( (a, b) => a - b );
-
-
-
-
-        if (checkbox.checked) {
-
-            clickAvailable = false;
-            /*finding the  position to place the checked list
-            checked list will be placed just above another already checked list(end) if none then it will be placed at buttom */
-            let start;
-            let end;
-            for (let i = keys.length - 1; i >= 0; i--) {
-                if (keys[i] == key) {
-                    start = i;
-                }
-                if (JSON.parse(localStorage.getItem(keys[i])).isChecked == true) {
-                    end = i;
-                    data.key = keys[i + 1];
-                    break;
-                } else {
-                    end = - 1;
-                    data.key = keys[0];
-                }
-            }
-            /*creating empty position for the checked list to be inserted
-            shifting will be done between the original position(start) of the checked list and the end position*/
-            for (let i = --start; i > end; i--) {
-
-
-                tempData.key = keys[i + 1];
-                tempData.input = JSON.parse(localStorage.getItem(keys[i])).input;
-                tempData.isChecked = JSON.parse(localStorage.getItem(keys[i])).isChecked;
-                localStorage.setItem(keys[i + 1], JSON.stringify(tempData));
-
-            }
-            /*after this loop there will be a duplicate element  at (end + 1) position
-            which will be replaced (outside of if-else block) by the list  which is unchecked  */
-
-            //animating
-            let ele = document.querySelector("#" + checkbox.id).parentElement.parentElement.parentElement;
-            ele.className = "listWithAnimation";
-            setTimeout(function () {
-                if (end != -1) {
-                    ele.parentNode.insertBefore(ele, ele.parentNode.children[(ele.parentNode.childElementCount - 1) - end]);
-                } else {
-                    ele.parentNode.appendChild(ele);
-                }
-            }, 300);
-
-            setTimeout(function () {
-                ele.className = ele.className + " show";
-            }, 350);
-
-
-
-        } else {
-
-            clickAvailable = false;
-            /*finding the  position to place the unchecked list
-            checked list will be placed just below another already unchecked list(start) if none then it will be placed at top */
-            let start;
-            let end;
-            for (let i = 0; i < keys.length; i++) {
-                if (keys[i] == key) {
-                    start = i;
-                }
-                if (JSON.parse(localStorage.getItem(keys[i])).isChecked == false) {
-                    end = i;
-                    data.key = keys[i - 1];
-                    break;
-                } else {
-                    end = keys.length;
-                    data.key = keys[keys.length - 1];
-                }
-            }
-
-            /*creating empty position for the checked list to be inserted
-            shifting will be done between the original position(start) of the cheched list and the end position*/
-            for (let i = ++start; i < end; i++) {
-
-                tempData.key = keys[i - 1];
-                tempData.input = JSON.parse(localStorage.getItem(keys[i])).input
-                tempData.isChecked = JSON.parse(localStorage.getItem(keys[i])).isChecked;
-                localStorage.setItem(keys[i - 1], JSON.stringify(tempData))
-            }
-            /*after this loop there will be a duplicate element  at (end - 1) position
-            which will be replaced (outside of if-else block) by the list  which is unchecked  */
-
-            //animating 
-            let ele = document.querySelector("#" + checkbox.id).parentElement.parentElement.parentElement;
-            ele.className = "listWithAnimation";
-            setTimeout(function () {
-                if (end != ele.parentNode.childElementCount) {
-                    ele.parentNode.insertBefore(ele, (ele.parentNode.children[(ele.parentNode.childElementCount - 1) - end]).nextSibling);
-                } else {
-                    ele.parentNode.prepend(ele);
-                }
-            }, 300);
-
-            setTimeout(function () {
-                ele.className = ele.className + " show";
-            }, 350);
-
-        }
-        localStorage.setItem(data.key, JSON.stringify(data));
-
-        setTimeout(function () {
-            update(false);
-            clickAvailable = true;
-        }, 600);
-
-    }
+    updateLocalStorage();
 }
 
-//function for adding record
-function addRecord(key, inputdata, checked, animate) {
-    let list = document.createElement('li')
-    list.className = 'list'
 
-    let container = document.querySelector('#uList')
-    container.prepend(list);
+function addTodoToScreen(id, isChecked, todoText, animate) {
+    let todosContainerNew = document.querySelector(".todosContainerNew")
+    let todosContainerDone = document.querySelector(".todosContainerDone")
 
-
-    //adding label
-    let label = document.createElement('label')
-    label.className = 'text'
-
-    list.appendChild(label);
-
-    //creating div
-    const div = document.createElement('div');
-    div.className = "round";
-
-    label.appendChild(div);
-
-    //adding checkbox
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.id = 'c' + key;// assigning id
-    checkbox.setAttribute('onclick', 'checkdone(this)')
-    checkbox.className = "check";
-    if (checked) {
-        checkbox.setAttribute('checked', '')//assigning boolean
-        label.classList = "linedLabel text";
-    }
-
-    div.appendChild(checkbox)
-
-    //adding label for checkbox
-    const label3 = document.createElement('label')
-    label3.setAttribute("for", 'c' + key)
-
-    div.appendChild(label3)
-
-    // adding text node 
-    let text = document.createTextNode(inputdata);// assigning todo data
-
-    div.appendChild(text)
-
+    let todo = document.createElement("li")
+    todo.className = "todo"
     if (animate) {
-        list.className = "listWithAnimation";
+        todo.className = "todo listWithAnimation";
         setTimeout(function () {
-            list.className = list.className + " show";
+            todo.className = todo.className + " show";
         }, 10);
     }
 
+    todo.innerHTML = `
+    <label>
+        <div class="todoContent">
+            <input type="checkbox" id="c${id}" onclick="todoStateChange(this)" ${isChecked ? "checked" : ""}>
+            <label for="c${id}"></label>
+            <div class="todoText">
+                <span >${todoText}</span>
+            </div>
+        </div>
+   </label>
+   <div class="contextMenu" tabindex="0">
+        <button type="button" title="edit" class="edit" onclick="editTodo(this)"></button>
+        <button type="button" title="delete" class="delete" onclick="deleteTodo(this)"></button>
+   </div>
+    `;
+    isChecked ? todosContainerDone.prepend(todo) : todosContainerNew.prepend(todo)
+}
+
+
+function handelSubmit(e) {
+
+    e.preventDefault();
+
+    let inputbox = document.querySelector('#inputbox')
+    if (!inputbox.value) return
+
+    let todoText = inputbox.value
+    let id = Date.now()
+    let isChecked = false;
+
+    if (editMode) {
+        tempCurrentTodo.querySelector(".todoText span").innerText = todoText;
+        id = tempCurrentTodo.querySelector("input[type='checkbox']").id.replace("c", "")
+        isChecked = tempCurrentTodo.querySelector(`#c${id}`).checked
+        editMode = false;
+
+    } else {
+
+        addTodoToScreen(id, isChecked, todoText, animate)
+    }
+
+    todos[id] = { id, isChecked, todoText };
+    updateLocalStorage();
+    inputbox.value = "";
+}
+
+
+function updateLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+
+function updateScreen() {
+    todos = JSON.parse(localStorage.getItem("todos"))
+    if (!todos) localStorage.setItem("todos", JSON.stringify({}))
+    let ids = Object.keys(todos).map((id => parseInt(id))).sort((a, b) => a - b)
+
+    ids.forEach((id) => {
+        addTodoToScreen(id, todos[id].isChecked, todos[id].todoText, doNotAnimation)
+    })
+
+    let hideCompleted = JSON.parse(localStorage.getItem("hideCompleted"))
+
+    if (hideCompleted) {
+        document.querySelector("#chk").checked = true;
+        document.querySelector(".todosContainerDone").style = "height:0px";
+        setTimeout(function () {
+            document.querySelector(".todosContainerDone").style = "";
+
+        }, 500);
+    }
+
+}
+
+function todoStateChange(checkbox) {
+    let todo = checkbox.parentElement.parentElement.parentElement
+    let oldTodoId = checkbox.id.replace("c", "")
+    let isChecked = checkbox.checked
+    let todoText = todos[oldTodoId].todoText
+    let id = Date.now()
+
+    delete todos[oldTodoId]
+    todos[id] = { id, isChecked, todoText };
+    checkbox.id = "c" + id;
+
+    if (isChecked) {
+        let todosContainerDone = document.querySelector(".todosContainerDone")
+        todosContainerDone.insertBefore(todo, todosContainerDone.children[0])
+    } else {
+        let todosContainerNew = document.querySelector(".todosContainerNew")
+        todosContainerNew.insertBefore(todo, todosContainerNew.children[0])
+    }
+
+    todo.className = "todo listWithAnimation";
+    setTimeout(function () {
+        todo.className = todo.className + " show";
+    }, 10);
+
+    updateLocalStorage();
+
+
+}
+
+function hideCompleted(checkbox) {
+    let isHidden = !!checkbox.checked
+    localStorage.setItem("hideCompleted", isHidden)
+
+}
+
+function deleteAllDoneTodo(){
+    let ids = Object.keys(todos).map((id => parseInt(id)))
+
+    ids.forEach((id) => {
+        if(todos[id].isChecked){
+            delete todos[id];
+           
+        }
+    })
+    updateLocalStorage();
+   document.querySelector(".todosContainerDone").innerHTML="";
 }
